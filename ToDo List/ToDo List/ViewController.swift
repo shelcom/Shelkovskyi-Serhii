19e9707abb502231dc729f2add069ae026149d63
Serhii <shelcom96@gmail.com>
2021-10-15 15:12:34 +0300

    Add UI elements

--- ToDo List/ToDo List/ViewController.swift
+++ ToDo List/ToDo List/ViewController.swift
@@ -12,6 +12,27 @@ class ViewController: UIViewController {
    override func viewDidLoad() {
       super.viewDidLoad()
       // Do any additional setup after loading the view.
+      print("viewDidLoad")
+   }
+   
+   override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      print("viewWillAppear")
+   }
+   
+   override func viewDidAppear(_ animated: Bool) {
+      super.viewDidAppear(animated)
+      print("viewDidAppear")
+   }
+   
+   override func viewDidDisappear(_ animated: Bool) {
+      super.viewDidDisappear(animated)
+      print("viewDidDisappear")
+   }
+   
+   override func viewWillDisappear(_ animated: Bool) {
+      super.viewWillDisappear(animated)
+      print("viewWillDisappear")
    }
 
 
